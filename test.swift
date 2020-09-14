19a208633c1d945347ab08022ad7ef996453cedb
bradenupchurch <56045105+bradenupchurch@users.noreply.github.com>
2020-09-14 10:55:30 -0600

    Add files via upload

---
+++ test.swift
@@ -0,0 +1,15 @@
+#!/usr/bin/swift
+
+var num = 0;
+print("Hello World")
+
+    if num == 0 {
+        print("I am grateful 1 time.")
+        num += 1;
+    }
+    while num < 5 {
+        num += 1; 
+        print("I am grateful \(num) times.")
+       
+    }
+
